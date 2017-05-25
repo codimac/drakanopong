d8d50118fda0a1753ad6d4e9f791a040cb7c9f04
nornei <noemie.pije@gmail.com>
2017-05-25 14:43:15 +0200

    manage collisions between ball and walls

--- src/ball.c
+++ src/ball.c
@@ -46,7 +46,6 @@ void animateBall(Ball * b, float time){
 }
 
 
-
 int collision(Ball * b){
 	if(b->center.x <= convertCoordToMark(-(float)WINDOW_WIDTH/2, WINDOW_WIDTH) 
 		|| b->center.x >= convertCoordToMark((float)WINDOW_WIDTH/2, WINDOW_WIDTH)){

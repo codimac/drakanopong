461d78bf6b286bed200dc54b7934e1cc3adb0bc2
laranyora <lilagazeau@gmail.com>
2017-09-19 21:24:52 +0200

    redondance entre texture et text suppriméé

--- src/text.c
+++ src/text.c
@@ -5,6 +5,7 @@
 #include "utils.h"
 #include "text.h"
 #include <SDL/SDL_ttf.h>
+
 #include "texture.h"
 
 void displayText(float x, float y, int width, int height, Text t){
@@ -22,46 +23,22 @@ void displayScore(int x, int y, int width, int height, Text t){
 }
 
 void drawText(Text * t){
-	glGenTextures(1, &t->id);
 	int colors;
-SDL_Color color={255,255,0};
-SDL_Surface * text_surface;
-TTF_Font *font;
-font = TTF_OpenFont("./assets/fonts/FreeSans.ttf", 10);
-if (font == NULL){
-	printf("Font is NULL");
-	exit(EXIT_FAILURE);
-}
-if(!(text_surface=TTF_RenderUTF8_Blended(font,t->string,color))) {
-	printf("TTF couldnt create texture");
-	exit(EXIT_FAILURE);
-}
-
-
-colors = text_surface->format->BytesPerPixel;
-	GLenum format;
-	if(text_surface != NULL){
-	    switch(text_surface->format->BytesPerPixel) {
-	    	case 1:
-	      		format = GL_ALPHA;
-	      	break;
-	      	case 3:
-	      		format = GL_RGB;
-	      	break;
-	      	case 4:
-	     		 format = GL_RGBA;
-	      	break;
-	      	default:
-			    fprintf(stderr, "Format des pixels de l'image non pris en charge\n");
-			    exit(EXIT_FAILURE);
-		}
+	SDL_Color color={255,255,0};
+	SDL_Surface * text_surface;
+	TTF_Font *font;
+	font = TTF_OpenFont("./assets/fonts/FreeSans.ttf", 10);
+	if (font == NULL){
+		printf("Font is NULL");
+		exit(EXIT_FAILURE);
 	}
-	glBindTexture(GL_TEXTURE_2D,t->id);
-	glTexImage2D( GL_TEXTURE_2D,0, colors/*format*/, text_surface->w, text_surface->h, 0, format, GL_UNSIGNED_BYTE, text_surface->pixels);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	if(!(text_surface=TTF_RenderUTF8_Blended(font,t->string,color))) {
+		printf("TTF couldnt create texture");
+		exit(EXIT_FAILURE);
+	}
+	colors = text_surface->format->BytesPerPixel;
+	createTexture(t, text_surface);
 	SDL_FreeSurface(text_surface);
-	glBindTexture(GL_TEXTURE_2D,0);
 }
 
     Text initText(char * string){
